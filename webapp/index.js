0de0a695b91e284b5f7eeed9457859fa7e5a55e9
stelladream1 <hyunonblue@naver.com>
2023-10-30 16:41:19 +0900

    Step 2: Creating a Model

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,20 @@
 sap.ui.require([
-	"sap/ui/core/Core",
-	"sap/m/Text"
-], function (
-	Core,
-	Text
-) {
+	"sap/m/Text",
+	"sap/ui/model/json/JSONModel"
+], function (Text, JSONModel) {
 	"use strict";
 
-	// Chain an anonymous function to the SAPUI5 'ready' Promise
-	Core.ready().then(function () {
+	// Attach an anonymous function to the SAPUI5 'init' event
+	sap.ui.getCore().attachInit(function () {
+		// Create a JSON model from an object literal
+		var oModel = new JSONModel({
+			greetingText: "Hi, my name is Harry Hawk"
+		});
+
+		// Assign the model object to the SAPUI5 core
+		sap.ui.getCore().setModel(oModel);
+
+
 		// Create a text UI element that displays a hardcoded text string
 		new Text({text: "Hi, my name is Harry Hawk"}).placeAt("content");
 	});
